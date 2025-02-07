31d19e905a7e0eb9e0a4411a40b69b9057608998
unknown <YY415@ccs.local>
2025-02-07 04:21:52 -0800

    Initialize Basic Web Application

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+//Serve static files from the "public" folder
+app.use(express.static('public'));
+
+app.use (express.json());
+
+app.post('submit',(req, res)=> {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 // Define a route for the home page
 app.get('/',(req, res)=> {
     res.send('Hello, World!');
 });
 
+app.get('/about',(req, res)=> {
+    res.send('About Us');
+});
 //Start the server
 app.listen(port, ()=> {
-    console.log('Server running at http://localhost:${port}');
+    console.log(`Server running at http://localhost:${port}`);
 });
